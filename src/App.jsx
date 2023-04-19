5f26721ba477a1e0b20693c3e21ca50cfa258568
Kevin <kevin.paez1010@gmail.com>
2023-04-18 18:23:03 -0600

    ordered folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-// import CounterApp from "./components/1Counter/CounterApp";
-// import TodoApp from "./components/2Todo/TodoApp";
-import ProductApp from "./components/3Product/ProductApp";
+// import CounterApp from "./1Counter/CounterApp";
+// import TodoApp from "./2Todo/TodoApp";
+import ProductApp from './3Product/ProductApp';
 
 function App() {
   return (
